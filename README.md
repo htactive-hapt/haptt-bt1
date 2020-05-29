03c992c1b1c09328522a894f72c6b7153435e1ca
unknown <Administrator@PNVD119.pn.com.vn>
2020-05-29 18:48:12 +0700

    thu 6

--- README.md
+++
@@ -1,4 +0,0 @@
-# hapttlab3
-Ôn tập html css through this link
-
-https://app.slack.com/client/T7UPLQBLY/C7VHLPR0D/files/F014ASSTPBL
--- js/script.js
+++ js/script.js
@@ -3,16 +3,23 @@ showSlides();
 
 function showSlides() {
     var i;
-    // var slides = document.getElementsByClassName("mySlides");
-    var images = document.getElementsByClassName("image");
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > images.length) {
-        slideIndex = 1;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
     }
-
-    images[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 5000);
+}
+
+function menuBarResponsive() {
+    var x = document.getElementById("hidden-menu-responsive");
+    if (x.className === "menu-bar") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu-bar";
+    }
 }
